91536fb5a2070d245cc2febac0dacd4a18ef5af4
Jedsada Tiwongvorakul <jedsada.t@20scoops.net>
2020-09-29 20:59:20 +0700

    updated code

--- scripts/validator.ts
+++ scripts/validator.ts
@@ -1,24 +1,46 @@
 import fs from "fs";
 import path from "path";
 import validate from "./schema.validator";
+import chalk from "chalk";
 
-const directoryPath = path.join(__dirname, "../projects");
+const directoryPath = path.join(__dirname, "./../projects");
+let isError = false;
 
-fs.readdir(directoryPath, (err, fodlers) => {
+fs.readdir(directoryPath, (err, dirs) => {
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
-  fodlers.forEach((subfolder) => {
-    const filePath = directoryPath + "/" + subfolder + "/info.json";
-    fs.readFile(filePath, "utf8", (err, data) => {
-      if (err) throw err;
-      try {
-        if (validate(JSON.parse(data))) {
-          console.log(`validated: ${filePath} is successfully`);
+  const validatedValues = dirs.map((subDirs) => {
+    const filePath = directoryPath + "/" + subDirs + "/info.json";
+    return new Promise((resolve, _) => {
+      fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) throw err;
+        try {
+          if (validate(JSON.parse(data))) {
+            resolve({
+              message: `validated: ${filePath} is successfully`,
+            });
+          }
+        } catch (err) {
+          isError = true;
+          resolve({
+            message: err,
+            isError: true,
+          });
         }
-      } catch (err) {
-          console.log(err)
-      }
+      });
     });
   });
+  Promise.all(validatedValues)
+    .then((values) => {
+      values.map((value: any) =>
+        console.log(
+          value.isError ? chalk.red(value.message) : chalk.green(value.message)
+        )
+      );
+      if (isError) process.exit(1);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
