694178d6a47c5344a849a587fb7465fb47340c10
Jedsada Tiwongvorakul <jedsada.t@20scoops.net>
2020-10-01 00:26:02 +0700

    implemented request to mutation assets

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,11 @@
 import fs from "fs";
 import path from "path";
+import dotenv from "dotenv";
+import { GraphQLClient, gql } from "graphql-request";
+import { Headers } from "cross-fetch";
+
+dotenv.config();
+global.Headers = global.Headers || Headers;
 
 const directoryPath = path.join(__dirname, "./../projects");
 
@@ -16,9 +22,28 @@ fs.readdir(directoryPath, (err, dirs) => {
     });
   });
   Promise.all(validatedValues)
-    .then((values) => {
-      // TODO: request api to update token
-      console.log(values);
+    .then(async (values) => {
+      const endpoint = process.env.MAIN_API_URL as string;
+      const graphQLClient = new GraphQLClient(endpoint);
+      graphQLClient.setHeaders({
+        authorization: `Bearer ${process.env.API_ASSETS_KET}`,
+      });
+
+      const mutation = gql`
+        mutation CrateAsset($data: [AssetInput]) {
+          createAsset(CreateAssetInput: { assets: $value }) {
+            [id]
+          }
+        }
+      `;
+
+      const variables = {
+        data: values,
+      };
+
+      const response = await graphQLClient.request(mutation, variables);
+      console.log(response);
+      if (!response) process.exit(1);
     })
     .catch((err) => {
       console.log(err);
