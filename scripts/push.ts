770df3ca91de596377ce54f421cfee9f4d62b175
seb0zz <sebastian@elbstack.com>
2020-10-31 15:26:03 +0100

    Debug bridge to backend repo

--- scripts/push.ts
+++ scripts/push.ts
@@ -16,6 +16,7 @@ fs.readdir(directoryPath, (err, _) => {
     return console.log("Unable to scan directory: " + err);
   }
 
+  console.log('changedFiles', JSON.stringify(changedFiles));
   const validatedValues = changedFiles.map((dir) => {
     const dirChange = dir.split("/", 3);
     if (dir.includes("projects") && dir.includes("json")) {
@@ -31,19 +32,21 @@ fs.readdir(directoryPath, (err, _) => {
       });
     }
   });
-  const filterNull = validatedValues.filter((data) => data);
-  Promise.all(filterNull)
-    .then(async (values) => {
-      const endpoint = process.env.MAIN_API_ENDPOINT as string;
-      if (!endpoint) {
-        throw new Error("API endpoint invalid");
-      }
-      const graphQLClient = new GraphQLClient(endpoint);
-      graphQLClient.setHeaders({
-        authorization: `Bearer ${process.env.API_ASSETS_KEY}`,
-      });
 
-      const mutation = gql`
+  const filterNull = validatedValues.filter((data) => data);
+  if (filterNull.length > 0) {
+    Promise.all(filterNull)
+      .then(async (values) => {
+        const endpoint = process.env.MAIN_API_ENDPOINT as string;
+        if (!endpoint) {
+          throw new Error("API endpoint invalid");
+        }
+        const graphQLClient = new GraphQLClient(endpoint);
+        graphQLClient.setHeaders({
+          authorization: `Bearer ${process.env.API_ASSETS_KEY}`,
+        });
+
+        const mutation = gql`
         mutation CrateAsset($data: [AssetGithub!]) {
           updateOrCreateAssetFromGithub(data: { assets: $data }) {
             id
@@ -51,16 +54,17 @@ fs.readdir(directoryPath, (err, _) => {
         }
       `;
 
-      const variables = {
-        data: values,
-      };
+        const variables = {
+          data: values,
+        };
 
-      const response = await graphQLClient.request(mutation, variables);
-      console.log(response);
-      if (!response) process.exit(1);
-    })
-    .catch((err) => {
-      console.log(err.message);
-      process.exit(1);
-    });
+        const response = await graphQLClient.request(mutation, variables);
+        console.log(response);
+        if (!response) process.exit(1);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        process.exit(1);
+      });
+  }
 });
