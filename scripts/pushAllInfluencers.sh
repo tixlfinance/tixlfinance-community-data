bf7d39f9a6cff644cc2ac19a8474c266aea48287
Jedsada Tiwongvorakul <jedsada.t@20scoops.net>
2020-12-28 20:00:53 +0700

    update path folder and change name script

--- scripts/pushAllInfluencers.sh
+++ scripts/pushAllInfluencers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # shellcheck disable=SC2045
-for entry in `ls ./influencers`; do
-  npm run push:influencers blogs/$entry/info.json
+for entry in `ls ./../influencers`; do
+  npm run push:influencers influencers/$entry/info.json
 done
 
