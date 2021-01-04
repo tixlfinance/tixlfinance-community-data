c65d713f75f14a2a9e1f8610fb31f48728dfe228
Leif <leif.leiser@gmail.com>
2021-01-04 21:16:44 +0100

    merge

--- scripts/pushNewsprovider.ts
+++ scripts/pushNewsprovider.ts
@@ -1,13 +1,13 @@
-import fs from 'fs';
-import path from 'path';
-import dotenv from 'dotenv';
-import { GraphQLClient, gql } from 'graphql-request';
-import { Headers } from 'cross-fetch';
+import fs from "fs";
+import path from "path";
+import dotenv from "dotenv";
+import { GraphQLClient, gql } from "graphql-request";
+import { Headers } from "cross-fetch";
 
 dotenv.config();
 global.Headers = global.Headers || Headers;
 
-const directoryPath = path.join(__dirname, './../newsprovider');
+const directoryPath = path.join(__dirname, "./../newsprovider");
 
 const changedFiles = process.argv.slice(2);
 
@@ -16,15 +16,15 @@ fs.readdir(directoryPath, (err, _) => {
     throw err;
   }
 
-  console.log('changedFiles', JSON.stringify(changedFiles));
+  console.log("changedFiles", JSON.stringify(changedFiles));
   const updatedNewsprovider = changedFiles
     .map((dir) => {
-      const dirChange = dir.split('/', 3);
-      if (dir.includes('newsprovider')) {
-        const filePath = directoryPath + '/' + dirChange[1] + '/info.json';
-        const logoPath = '/newsprovider/' + dirChange[1] + '/logo.png';
+      const dirChange = dir.split("/", 3);
+      if (dir.includes("newsprovider")) {
+        const filePath = directoryPath + "/" + dirChange[1] + "/info.json";
+        const logoPath = "/newsprovider/" + dirChange[1] + "/logo.png";
         return new Promise((resolve, _) => {
-          fs.readFile(filePath, 'utf8', (_, data) => {
+          fs.readFile(filePath, "utf8", (_, data) => {
             if (data) {
               const parsed = JSON.parse(data);
               resolve({
@@ -42,12 +42,12 @@ fs.readdir(directoryPath, (err, _) => {
     Promise.all(updatedNewsprovider)
       .then(async (newsproviders: any[]) => {
         for (const newsprovider of newsproviders) {
-          console.log('Processing category', JSON.stringify(newsprovider));
+          console.log("Processing category", JSON.stringify(newsprovider));
           delete newsprovider.__triggerUpdate;
 
           const endpoint = process.env.MAIN_API_ENDPOINT as string;
           if (!endpoint) {
-            throw new Error('API endpoint invalid');
+            throw new Error("API endpoint invalid");
           }
           const graphQLClient = new GraphQLClient(endpoint);
           graphQLClient.setHeaders({
@@ -61,8 +61,8 @@ fs.readdir(directoryPath, (err, _) => {
           const existsResponse = await graphQLClient.request(existsQuery);
           const alreadyExists = existsResponse.newsProviderById !== null;
           const mutationToUse = alreadyExists
-            ? 'updateNewsProviderFromGithub'
-            : 'createNewsProviderFromGithub';
+            ? "updateNewsProviderFromGithub"
+            : "createNewsProviderFromGithub";
 
           const mutation = gql`
                     mutation CreateNewsprovider($data: NewsProviderInput!) {
@@ -78,10 +78,10 @@ fs.readdir(directoryPath, (err, _) => {
 
           const response = await graphQLClient.request(mutation, variables);
           if (!response) {
-            throw new Error('No response from mutation call');
+            throw new Error("No response from mutation call");
           }
           console.info(
-            `${alreadyExists ? 'Updated' : 'Created'} category ${
+            `${alreadyExists ? "Updated" : "Created"} category ${
               newsprovider.url
             }`
           );
