efeceabc311d2d85a4c8aad4580e6caccd7ba846
Leif <leif.leiser@gmail.com>
2021-01-26 12:48:40 +0100

    lint

--- scripts/pushIssuesAndPRs.ts
+++ scripts/pushIssuesAndPRs.ts
@@ -5,53 +5,53 @@ import { GraphQLClient, gql } from "graphql-request";
 dotenv.config();
 
 async function main() {
-    const authToken = process.env.MAIN_API_TOKEN as string;
-    if (!authToken) {
-        throw new Error("API token invalid");
+  const authToken = process.env.MAIN_API_TOKEN as string;
+  if (!authToken) {
+    throw new Error("API token invalid");
+  }
+
+  const endpoint = process.env.MAIN_API_ENDPOINT as string;
+  if (!endpoint) {
+    throw new Error("API endpoint invalid");
+  }
+
+  const graphQLClient = new GraphQLClient(endpoint);
+  graphQLClient.setHeaders({ authorization: `Bearer ${authToken}` });
+
+  const path = process.env.GITHUB_EVENT_PATH as string;
+  const event = JSON.parse(fs.readFileSync(path, "utf8"));
+
+  console.log(event);
+
+  const mutation = gql`
+    mutation upsert($issue: IssueInput!) {
+      upsertIssue(data: $issue) {
+        _id
+        created_at
+        updated_at
+        labels
+        github_id
+        title
+        url
+        user_id
+        transaction_id
+      }
     }
+  `;
 
-    const endpoint = process.env.MAIN_API_ENDPOINT as string;
-    if (!endpoint) {
-        throw new Error("API endpoint invalid");
-    }
+  const issue = {
+    github_id: event.issue.id.toString(),
+    title: event.issue.title,
+    labels: event.issue.labels.map((label) => label.name),
+    url: event.issue.url,
+  };
 
-    const graphQLClient = new GraphQLClient(endpoint);
-    graphQLClient.setHeaders({ authorization: `Bearer ${authToken}` });
+  const response = await graphQLClient.request(mutation, { issue });
+  if (!response) {
+    throw new Error("No response from mutation call");
+  }
 
-    const path = process.env.GITHUB_EVENT_PATH as string;
-    const event = JSON.parse(fs.readFileSync(path, "utf8"));
-
-    console.log(event);
-
-    const mutation = gql`
-        mutation upsert($issue: IssueInput!) {
-            upsertIssue(data: $issue) {
-                _id
-                created_at
-                updated_at
-                labels
-                github_id
-                title
-                url
-                user_id
-                transaction_id
-            }
-        }
-    `;
-
-    const issue = {
-        github_id: event.issue.id.toString(),
-        title: event.issue.title,
-        labels: event.issue.labels.map((label) => label.name),
-        url: event.issue.url,
-    };
-
-    const response = await graphQLClient.request(mutation, { issue });
-    if (!response) {
-        throw new Error("No response from mutation call");
-    }
-
-    console.log("It worked!: ", response);
+  console.log("It worked!: ", response);
 }
 
 main();
