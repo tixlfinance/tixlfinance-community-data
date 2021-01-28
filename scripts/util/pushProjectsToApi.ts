98593472392c00ee0f7d7a753d4388b6132120f9
Leif <leif.leiser@gmail.com>
2021-01-28 17:44:30 +0100

    improved flow

--- scripts/util/pushProjectsToApi.ts
+++ scripts/util/pushProjectsToApi.ts
@@ -1,14 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import dotenv from 'dotenv';
-import { GraphQLClient, gql } from 'graphql-request';
-import { Headers } from 'cross-fetch';
+import fs from "fs";
+import path from "path";
+import dotenv from "dotenv";
+import { GraphQLClient, gql } from "graphql-request";
+import { Headers } from "cross-fetch";
 
 export const pushProjects = async (isPreview?: boolean) => {
   dotenv.config();
   global.Headers = global.Headers || Headers;
 
-  const directoryPath = path.join(__dirname, './../../projects');
+  const directoryPath = path.join(__dirname, "./../../projects");
 
   const changedFiles = process.argv.slice(2);
 
@@ -17,19 +17,19 @@ export const pushProjects = async (isPreview?: boolean) => {
       throw err;
     }
 
-    console.log('changedFiles', JSON.stringify(changedFiles));
+    console.log("changedFiles", JSON.stringify(changedFiles));
     const updatedProjects = changedFiles
       .map((dir) => {
-        const dirChange = dir.split('/', 3);
-        if (dir.includes('projects')) {
-          const filePath = directoryPath + '/' + dirChange[1] + '/info.json';
-          const logoPath = '/projects/' + dirChange[1] + '/logo.png';
+        const dirChange = dir.split("/", 3);
+        if (dir.includes("projects")) {
+          const filePath = directoryPath + "/" + dirChange[1] + "/info.json";
+          const logoPath = "/projects/" + dirChange[1] + "/logo.png";
           const descriptionPath =
-            directoryPath + '/' + dirChange[1] + '/description.md';
+            directoryPath + "/" + dirChange[1] + "/description.md";
 
           return new Promise((resolve, _) => {
-            fs.readFile(filePath, 'utf8', (_, data) => {
-              fs.readFile(descriptionPath, 'utf8', (_, descriptionData) => {
+            fs.readFile(filePath, "utf8", (_, data) => {
+              fs.readFile(descriptionPath, "utf8", (_, descriptionData) => {
                 if (data) {
                   const parsed = {
                     ...JSON.parse(data),
@@ -38,7 +38,7 @@ export const pushProjects = async (isPreview?: boolean) => {
                   resolve({
                     ...parsed,
                     logo: parsed.logo || logoPath,
-                    asset_id: `${dirChange[1]}${isPreview ? '-preview' : ''}`,
+                    asset_id: `${dirChange[1]}${isPreview ? "-preview" : ""}`,
                     isPreview,
                     description_markdown:
                       parsed.description_markdown || descriptionPath,
@@ -57,12 +57,12 @@ export const pushProjects = async (isPreview?: boolean) => {
       Promise.all(updatedProjects)
         .then(async (projects: any[]) => {
           for (const project of projects) {
-            console.log('Processing project', JSON.stringify(project));
+            console.log("Processing project", JSON.stringify(project));
             delete project.__triggerUpdate;
 
             const endpoint = process.env.MAIN_API_ENDPOINT as string;
             if (!endpoint) {
-              throw new Error('API endpoint invalid');
+              throw new Error("API endpoint invalid");
             }
             const graphQLClient = new GraphQLClient(endpoint);
             graphQLClient.setHeaders({
@@ -90,13 +90,13 @@ export const pushProjects = async (isPreview?: boolean) => {
               const removeResponse = await graphQLClient.request(
                 removePreviousPreviewBuildQuery
               );
-              console.log('removeResponse', removeResponse);
+              console.log("removeResponse", removeResponse);
               alreadyExists = false;
             }
 
             const mutationToUse = alreadyExists
-              ? 'updateAssetFromGithub'
-              : 'createAssetFromGithub';
+              ? "updateAssetFromGithub"
+              : "createAssetFromGithub";
 
             const mutation = gql`
               mutation CreateAsset($data: AssetInput!) {
@@ -112,11 +112,11 @@ export const pushProjects = async (isPreview?: boolean) => {
 
             const response = await graphQLClient.request(mutation, variables);
             if (!response) {
-              throw new Error('No response from mutation call');
+              throw new Error("No response from mutation call");
             }
 
             console.info(
-              `${alreadyExists ? 'Updated' : 'Created'} project ${
+              `${alreadyExists ? "Updated" : "Created"} project ${
                 project.asset_id
               }`
             );
